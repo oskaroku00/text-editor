f8a66f0ec7d65984cde9658b11943644d26783f7
oskaroku00 <138238892+oskaroku00@users.noreply.github.com>
2024-10-04 10:47:03 +0200

    Add files via upload

--- editor.c
+++ editor.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <dirent.h>
 
 void informacion(char* buffer);
 int list();
@@ -42,6 +42,7 @@ int main(){
 }
 
 void informacion(char* buffer){
+    printf("\n\n\n");
     printf("Que quieres hacer?\n\n");
     printf("Listar los archivos en el directorio: l\n");
     printf("Crear un archivo: c\n");
@@ -51,13 +52,51 @@ void informacion(char* buffer){
     printf("Que eliges? : ");
     // save the user input in buffer
     scanf("%1s", buffer);
+    printf("\n\n\n");
 }
 
 int list(){
+    // opendir() returns a pointer of DIR type.
+    // defino un puntero hacia un directorio
+    DIR *dr = opendir(".");
+    //defino una estructura que almacena el contenido del directorio
+    struct dirent *de;
 
+
+    if (dr == NULL)  // opendir returns NULL if couldn't open directory
+    {
+        printf("Hubo un error al abrir la carpeta");
+        return 1;
+    }
+    int contador = 0;
+    // for readdir()
+    while ((de = readdir(dr)) != NULL){
+        if (de->d_name[1] != '.' && de->d_name[0] != '.'){
+            printf("%d   %s\n", contador, de->d_name);
+            contador++;
+        }
+    }
+    //se cierra el directorio
+    closedir(dr);
+    return 0;
 }
 int create(){
-
+    //preguntar por el nombre del archivo
+    char nombre[64];
+    printf("Escribe el nombre del archivo : ");
+    // save the user input in buffer
+    scanf("%s", &nombre);
+    if (nombre[0] == NULL){
+        printf("El nombre del archivo esta vacio o es invalido");
+        return 1;
+    }
+    //crear el archivo con la funcion FOPEN
+    FILE* archivo = fopen(nombre, "w");
+    //cerrar el archivo
+    fclose(archivo);
+    //listar los directorios
+    list();
+    rerurn 0;
 }
 int edit(){
 
